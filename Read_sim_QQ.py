38e037ac4c082e9d9bfaf751a3be97c540157e32
Paul Elkson <32803572+PaulLos@users.noreply.github.com>
2018-10-19 00:58:00 +0300

    Update and rename Read_sim.py to Read_sim_QQ.py

--- Read_sim_QQ.py
+++ Read_sim_QQ.py
@@ -1,7 +1,27 @@
 import time
 import RPi.GPIO as GPIO
 import MFRC522
- 
+import json
+import urllib
+import urllib2
+
+# QQ setup
+def query(url, payload):
+    url = 'https://api.quickquest.ru/stock/'+url
+    data = json.dumps(payload)
+
+    request = urllib2.Request(url, data)
+    request.add_header('Authorization', 'Token 2222222222222222222222222222222222222222')
+    
+    try:
+        answer = json.loads(urllib2.urlopen(request).read())
+        code = 200
+    except urllib2.HTTPError as e:
+        code = e.code
+        answer = ''
+    return_array = {"data": answer, "http_code": code}
+    return return_array
+   
 # Create an object of the class MFRC522
 MIFAREReader = MFRC522.MFRC522()
  
@@ -26,7 +46,7 @@ try:
       # Print UID
       print("UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
  
-      time.sleep(2)
+      time.sleep(3)
  
 except KeyboardInterrupt:
   GPIO.cleanup()
